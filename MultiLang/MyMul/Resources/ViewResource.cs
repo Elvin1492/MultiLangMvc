4950c8a6f67afc35a8752dee7de6aa774b3139a9
Elvin_1492 <elvin.arzumanoglu@gmail.com>
2019-01-15 12:53:09 +0300

    akuna

---
+++ MultiLang/MyMul/Resources/ViewResource.cs
@@ -0,0 +1,7 @@
+﻿namespace MyMul.Resources
+{
+    // dummy class for grouping localizarion resources
+    public class ViewResource
+    {
+    }
+}
---
+++ MultiLang/MyMul/Startup.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyMul.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyMul.Utilities;
+using MyMul.Resources;
+using System.Reflection;
+using Microsoft.Extensions.Localization;
+
+namespace MyMul
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddDefaultUI(UIFramework.Bootstrap4)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.ConfigureRequestLocalization();
+
+            services.AddMvc()
+                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddViewLocalization(o => o.ResourcesPath = "Resources")
+                .AddModelBindingMessagesLocalizer(services)
+                // Option A: use this for localization with shared resource
+                .AddDataAnnotationsLocalization(o => {
+                    var type = typeof(ViewResource);
+                    var assemblyName = new AssemblyName(type.GetTypeInfo().Assembly.FullName);
+                    var factory = services.BuildServiceProvider().GetService<IStringLocalizerFactory>();
+                    var localizer = factory.Create("ViewResource", assemblyName.Name);
+                    o.DataAnnotationLocalizerProvider = (t, f) => localizer;
+                })
+                 .AddRazorPagesOptions(o =>
+                 {
+                     o.Conventions.Add(new CultureTemplateRouteModelConvention());
+                 });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseRequestLocalization();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{culture?}/{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
